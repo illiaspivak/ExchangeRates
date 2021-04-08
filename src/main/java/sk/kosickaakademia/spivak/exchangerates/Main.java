80500d1f8ab22085f7ee792135d2cef4f4ca84cb
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-04-08 09:52:05 +0200

    New project Exchange Rates

---
+++ src/main/java/sk/kosickaakademia/spivak/exchangerates/Main.java
@@ -0,0 +1,13 @@
+package sk.kosickaakademia.spivak.exchangerates;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
