73a8bcd26c80c4933ac9c70a1e1f1e828b501ec0
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-04-14 09:51:33 +0200

    change Controller

--- src/main/java/sk/kosickaakademia/spivak/exchangerates/gui/Controller.java
+++ src/main/java/sk/kosickaakademia/spivak/exchangerates/gui/Controller.java
@@ -7,8 +7,12 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import sk.kosickaakademia.spivak.exchangerates.calculator.Calculator;
 
+import java.text.DecimalFormat;
+import java.util.Map;
+
 public class Controller {
 
+    private static final String[] rates= new String[]{"UAH","RUB","KZT","VEF","BTC"};
     public TextField amountOfMoney;
     public Button convert;
     public TextField convertUAH;
@@ -18,6 +22,20 @@ public class Controller {
     public TextField convertBTC;
 
     public void Convert(ActionEvent actionEvent) {
+        String value = amountOfMoney.getText();
+        double valueEur=Double.parseDouble(value);
+        Calculator calculator =new Calculator();
+        Map results = calculator.calculate(valueEur,rates);
+        convertUAH.setText(convertTo2Decimal((double)results.get("UAH")));
+        convertRUB.setText(convertTo2Decimal((double)results.get("RUB")));
+        convertKZT.setText(convertTo2Decimal((double)results.get("KZT")));
+        convertVEF.setText(convertTo2Decimal((double)results.get("VEF")));
+        convertBTC.setText(results.get("BTC").toString());
+    }
 
+    private String convertTo2Decimal(double value){
+        DecimalFormat df = new DecimalFormat("#.00");
+        String angleFormated = df.format(value);
+        return angleFormated;
     }
 }
